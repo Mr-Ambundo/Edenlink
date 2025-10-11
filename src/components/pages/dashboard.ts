7f51b2b3b8111ee62c65abc037cf9140a264324b
Mr-Ambundo <Theambundo@gmail.com>
2025-10-11 19:18:12 +0300

    Updated dashboard.ts

--- src/components/pages/dashboard.ts
+++ src/components/pages/dashboard.ts
@@ -50,10 +50,8 @@ export default class DashboardPage {
         </div>
 
         <!-- Metrics Grid -->
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6">
-          <div id="metrics-container">
-            <!-- Metric cards will be inserted here -->
-          </div>
+        <div id="metrics-container" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4">
+          <!-- Metric cards will be inserted here -->
         </div>
 
         <!-- Charts Section -->
